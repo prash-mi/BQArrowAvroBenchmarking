ed7ea735df70f6f87e1723f759b72ddc4275b868
Prashant Mishra <prash_mi@yahoo.com>
2021-08-24 08:36:09 +0530

    Added Constants file

---
+++ src/main/java/com/google/cloud/bigquery/benchmark/Constants.java
@@ -0,0 +1,15 @@
+package com.google.cloud.bigquery.benchmark;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+public class Constants {
+    public static final String SRC_TABLE = //"projects/bigquery-public-data/datasets/usa_names/tables/usa_1910_current";
+            "projects/bigquery-public-data/datasets/new_york_taxi_trips/tables/tlc_yellow_trips_2017";
+
+    public static  final List<String> FIELDS = ImmutableList.of("vendor_id","pickup_datetime","rate_code","dropoff_datetime","payment_type","pickup_location_id","dropoff_location_id");//,"dropoff_datetime","passenger_count","trip_distance","trip_distance","pickup_latitude","dropoff_longitude","payment_type","fare_amount","extra","tip_amount","imp_surcharge","pickup_location_id","dropoff_location_id");
+    public static  final String PROJECT_ID = "java-docs-samples-testing";
+    public static final int WARMUP_ITERATIONS = 2;
+    public static final int MEASUREMENT_ITERATIONS = 5;
+}
