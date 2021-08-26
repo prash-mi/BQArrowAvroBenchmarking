e6ce518529e14d7601a10fab309d64bccb7fcdd1
Prashant Mishra <inbox.prashant@gmail.com>
2021-08-26 08:17:42 +0530

    Update Constants

--- src/main/java/com/google/cloud/bigquery/benchmark/Constants.java
+++ src/main/java/com/google/cloud/bigquery/benchmark/Constants.java
@@ -9,6 +9,13 @@ public class Constants {
             "projects/bigquery-public-data/datasets/new_york_taxi_trips/tables/tlc_yellow_trips_2017";
 
     public static  final List<String> FIELDS = ImmutableList.of("vendor_id","pickup_datetime","rate_code","dropoff_datetime","payment_type","pickup_location_id","dropoff_location_id");//,"dropoff_datetime","passenger_count","trip_distance","trip_distance","pickup_latitude","dropoff_longitude","payment_type","fare_amount","extra","tip_amount","imp_surcharge","pickup_location_id","dropoff_location_id");
+     /*
+     //All the 21 fields
+     public static  final List<String> FIELDS = ImmutableList.of("vendor_id","pickup_datetime","dropoff_datetime","passenger_count","trip_distance",
+            "pickup_longitude","pickup_latitude","rate_code","store_and_fwd_flag","dropoff_longitude",
+            "dropoff_latitude","payment_type","fare_amount","extra","mta_tax","tip_amount"
+            ,"tolls_amount","imp_surcharge","total_amount","pickup_location_id","dropoff_location_id");
+            */
     public static  final String PROJECT_ID = "java-docs-samples-testing";
     public static final int WARMUP_ITERATIONS = 2;
     public static final int MEASUREMENT_ITERATIONS = 5;
